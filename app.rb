53a184343fcfae1136d5d5cd401a99a9fb82acb2
Michael Manwaring <Michael.c.Manwaring@gmail.com>
2015-12-29 11:21:00 -0500

    tuesday morning commit

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ set :database, "sqlite3:testdata.sqlite3"
 set :sessions, true
 
 # functions
-current_user = User.find(1)
+# current_user = User.find(1)
 # def current_user
 # 	if session[:user_id]
 # 		User.find(session[:user_id])
@@ -64,14 +64,14 @@ post '/sign_up' do
 end
 
 post '/sign_in' do
-	@user = User.where(username: params[:username]).first
-	if @user && @user.password == params[:password]
-    	session[:user_id] = @user.id
-  	else
-	    flash[:alert] = "Are you sure you have the correct username and password?"
-    	redirect '/'
-	end
-  	redirect '/home'
+	# @user = User.where(username: params[:username]).first
+	# if @user && @user.password == params[:password]
+ #    	session[:user_id] = @user.id
+ #  	else
+	#     flash[:alert] = "Are you sure you have the correct username and password?"
+ #    	redirect '/'
+	# end
+ #  	redirect '/home'
 end
 
 post '/sign_out' do
