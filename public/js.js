d0f714a6598f0ce027da3af599ad3a82de776393
Kyle McCloskey <mccloskeykyle@gmail.com>
2015-12-29 15:15:11 -0500

    Added layout to the followers page

--- public/js.js
+++ public/js.js
@@ -68,4 +68,12 @@ $( "#signinform" ).submit(function( event ) {
       $(".post_box").slideUp(800)
     });
   });
+  // text counter for mumble box
+  var text_max = 150;
+  $('#textarea_feedback').html(text_max + ' characters remaining');
+  $('#textarea').keyup(function() {
+    var text_length = $('#textarea').val().length;
+    var text_remaining = text_max - text_length;
+  $('#textarea_feedback').html(text_remaining + ' characters remaining');
+    });
 });
