a8ba8ce06483e70130409e65b89b55227c5acc59
Kyle McCloskey <mccloskeykyle@gmail.com>
2016-01-01 13:54:42 -0500

    fixed margin error with nav list and modified the route on new posts to redirect to profile

--- app.rb
+++ app.rb
@@ -124,5 +124,5 @@ post '/mumbl' do
 	mumbl(params[:mumbl], new_post)
 	# content of post is changed to mumblfied "new_post".
 	Post.create(content:new_post, user_id: current_user.id)
-
+	redirect '/profile'
 end
