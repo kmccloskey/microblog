500c92712b188e9a2793c33874c517812dea8cca
Michael Manwaring <Michael.c.Manwaring@gmail.com>
2016-01-04 10:39:18 -0500

    adding heroku things 5

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require 'sinatra'
 require 'sinatra/activerecord'
 require './models'
 configure(:development){set :database, "sqlite3:blog.sqlite3"}
-set :database, "sqlite3:testdata.sqlite3"
+# set :database, "sqlite3:testdata.sqlite3"
 set :sessions, true
 
 require 'bundler/setup' 
